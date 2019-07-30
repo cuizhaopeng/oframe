997762ade4e468f6ad797d8676bfc55a1729f9ce
cui <403778642@qq.com>
2019-07-30 18:14:32 +0800

    压缩解压缩完成

--- application/admin/controller/UtilityFunction.php
+++ application/admin/controller/UtilityFunction.php
@@ -9,8 +9,12 @@
 namespace app\admin\controller;
 
 
+use app\admin\model\MhEpisodes;
+use app\admin\model\MhList;
+use app\lib\exception\ErrorMessage;
 use think\Controller;
 use think\facade\Request;
+use think\File;
 
 class UtilityFunction extends Controller
 {
@@ -24,29 +28,44 @@ class UtilityFunction extends Controller
     {
 
     }
+
+    /**
+     * 检查目录是否可写
+     * @access protected
+     * @param  string   $path    目录
+     * @return boolean
+     */
+    private function checkPath($path)
+    {
+        if (is_dir($path)) {
+            return true;
+        }
+
+        if (mkdir($path, 0755, true)) {
+            return true;
+        }
+
+        return new ErrorMessage();
+    }
+
     //  压缩解压
     public function compression()
     {
+
         // 判断请求的类型
         if (!Request::isPost()) return $this->fetch();
         //若上传了分集压缩包  接受上传的压缩包
         $file = Request::file('file');
-        // 生成当天日期的文件夹，检测文件夹是否存在，如果不存在创建
-        $savename = './uploads-new/' . date('Ymd');
-        $file->checkPath($savename);
-        if (is_dir($savename)) {
-            return true;
-        }
 
-        if (mkdir($savename, 0755, true)) {
-            return true;
-        }
+        // 生成当天日期的文件夹，检测文件夹是否存在，如果不存在创建
+        $savename = './uploads-new/' . date('Ymd') .DIRECTORY_SEPARATOR. md5(time().rand()).DIRECTORY_SEPARATOR;
+        if (!$this->checkPath($savename)) return new ErrorMessage();
+
+        // 验证规则以及源文件保存到相关目录
         $info = $file->validate(['size'=>200*1024*1024,'ext'=>'zip,rar'])->move( './uploads');
         if($info){
             // 成功上传后 获取上传信息
             $path = $_SERVER['DOCUMENT_ROOT'].'/uploads/'.str_replace("\\","/",$info->getSaveName());;
-//            $path = $_SERVER['DOCUMENT_ROOT'] . $info['savepath'];
-            $file = $path . $info->getFilename();
 
             // 打开压缩文件
             $zip = new \ZipArchive();
@@ -60,16 +79,17 @@ class UtilityFunction extends Controller
                 $encode = mb_detect_encoding($fileName, array("ASCII", "UTF-8", "GB2312", "GBK", "BIG5"));
                 $file_name = mb_convert_encoding($fileName, 'UTF-8', $encode);
                 $source = "zip://" . $path . "#" . $fileNameU; //源地址路径
-                $dest = $_SERVER['DOCUMENT_ROOT'] . '/uploads-new/' . $file_name; // 需要保存压缩内容的路径
+                $dest = $_SERVER['DOCUMENT_ROOT'] . $savename. DIRECTORY_SEPARATOR . $file_name; // 需要保存压缩内容的路径
                 copy($source, $dest);
             }
             $zip->close();
-            unlink($path);
-            $_POST['cert'] = $path;
+//            unlink($path);
+//            $_POST['cert'] = $path;
         }else{
             // 上传失败获取错误信息
             echo $file->getError();
         }
+        $this->addEpisodes($_SERVER['DOCUMENT_ROOT'] . $savename,198);
 
     }
 
@@ -114,8 +134,8 @@ class UtilityFunction extends Controller
                         "create_time"=>time(),
                         "update_time"=>0,
                     );
-                    M('mh_list')->where(array('id'=>$mhid))->save(array('episodes'=>$i));
-                    M('mh_episodes')->add($ds);
+                    $res = (new MhList())->save(['episodes'=>$i],['id'=>$mhid]);
+                    $res = (new MhEpisodes())->save($ds);
                     $i++;
                 }
             }
---
+++ application/admin/model/MhEpisodes.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/4/9
+ * Time: 18:41
+ */
+
+namespace app\admin\model;
+
+
+use think\Model;
+
+class MhEpisodes extends Model
+{
+
+}
